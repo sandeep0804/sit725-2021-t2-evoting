4696f836331b9824810726649f36035d86ba69e2
Nitin Singh <singhnitin755@gmail.com>
2021-10-07 22:17:59 +0530

    new additions

--- server.js
+++ server.js
@@ -1,13 +1,36 @@
-var express = require("express");
-var app = express()
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const { Server } = require('socket.io');
+const socketController = require('./controllers/socketController');
 
+const PORT = process.env.PORT || 3000;
+const app = express();
 
-app.use(express.static(__dirname + '/public'))
-app.use(express.json());
+// Initialize Socket.IO server
+const server = http.createServer(app);
+const io = new Server(server);
+
+// Serve the static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Parse the body for POST and PUT requests
 app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
-var port = process.env.port || 3000;
+// API routes
+app.use('/api/candidates', require('./routes/candidates'));
+app.use('/api/elections', require('./routes/elections'));
+app.use('/api/voters', require('./routes/voters'));
+app.use('/api/votes', require('./routes/votes'));
 
-app.listen(port, () => {
-    console.log("Application has started on port: ", port)
-})
+// Routes
+app.get('/', (req, res) => res.sendFile(path.resolve('public', 'index.html')));
+
+// Socket connection event handler
+io.on('connection', (socket) => {
+  console.log('Client connected', socket.id);
+  socketController(io, socket);
+});
+
+server.listen(PORT, () => console.log(`Server started on port ${PORT}`));
